2e8b2d59198872493caa90c62c56d9c4a952bb90
pokmeash <69669472+pokmeash@users.noreply.github.com>
2021-08-17 10:27:06 +0800

    updating

--- App/Source/Scene2D/RoomManager.cpp
+++ App/Source/Scene2D/RoomManager.cpp
@@ -1,112 +1,52 @@
-//#include "RoomManager.h"
-//#include <iostream>
-//using namespace std;
-//
-//// Include Shader Manager
-//#include "RenderControl\ShaderManager.h"
-//
-//#include "System\filesystem.h"
-//
-//
-//
-///**
-// @brief Constructor This constructor has protected access modifier as this class will be a Singleton
-// */
-//CRoomManager2D::CRoomManager2D(void)
-//{
-//	cursor = 0;
-//	current = first;
-//}
-//
-///**
-// @brief Destructor
-// */
-//CRoomManager2D::~CRoomManager2D(void)
-//{
-//	//delete nodes in deconstructor
-//	CRoomNode* tempcurrent = first;
-//	while (tempcurrent != nullptr)
-//	{
-//		CRoomNode* NodeDelete = tempcurrent;
-//		tempcurrent = tempcurrent->next;
-//
-//		delete NodeDelete;
-//	}
-//}
-//
-//bool CRoomManager2D::getprev()
-//{
-//	if (current->prev == nullptr)
-//	{
-//		//if prev is null cant go prev so return false/error
-//		return false;
-//	}
-//	else
-//	{
-//		//Code for cursor to go prev
-//		current = current->prev;
-//		cursor--;
-//		return true;
-//	}
-//
-//}
-//
-//bool CRoomManager2D::getnext()
-//{
-//	if (current->next == nullptr)
-//	{
-//		//if next is null cant go next so return false/error
-//		return false;
-//	}
-//	else
-//	{
-//		//Code for cursor to go next
-//		current = current->next;
-//		cursor++;
-//		return true;
-//	}
-//}
-//
-//void CRoomManager2D::getadd()
-//{
-//	newnode = new CNode;
-//	if (current == nullptr)
-//	{
-//		first = last = current = newnode;
-//	}
-//	else if (current->val == 'E')
-//	{
-//		//check for end node 
-//		current->next = newnode;
-//		newnode->prev = current;
-//		last = newnode;
-//
-//		current->next->val = 'E';
-//		current->val = 'o';
-//
-//	}
-//	else
-//	{
-//		//with temp
-//
-//		/*CNode* newnode = new CNode;
-//		CNode* tempnode = current->next;
-//		current->next = newnode;
-//		newnode->val = 'w';
-//		newnode->next = tempnode;
-//		tempnode->prev = newnode;
-//		newnode->prev = current;*/
-//
-//		//without temp
-//
-//		newnode->next = current->next;
-//		newnode->prev = current;
-//
-//		current->next->prev = newnode;
-//		current->next = newnode;
-//
-//		newnode->val = 'w';
-//
-//	}
-//}
-//
+#include "RoomManager.h"
+#include <iostream>
+using namespace std;
+
+// Include Shader Manager
+#include "RenderControl\ShaderManager.h"
+
+#include "System\filesystem.h"
+
+
+
+/**
+ @brief Constructor This constructor has protected access modifier as this class will be a Singleton
+ */
+CRoomManager2D::CRoomManager2D(void)
+{
+	cursor = 1;
+}
+
+/**
+ @brief Destructor
+ */
+CRoomManager2D::~CRoomManager2D(void)
+{
+
+}
+
+bool CRoomManager2D::getprev()
+{
+	cursor--;
+}
+
+bool CRoomManager2D::getnext()
+{
+	cursor++;
+}
+
+void CRoomManager2D::GenerateRoom()
+{
+	int Randint = rand() % 100 + 1; // rand int between 1 - 100
+	cMap2D->SetCurrentLevel(Randint);
+}
+
+void CRoomManager2D::NextRoom(int door)
+{
+
+}
+
+void CRoomManager2D::currentRoom()
+{
+	cMap2D->SetCurrentLevel(cursor);
+}
--- App/Source/Scene2D/RoomManager.h
+++ App/Source/Scene2D/RoomManager.h
@@ -5,6 +5,7 @@
 // Include vector
 #include <vector>
 #include "EntityManager.h"
+#include "Map2D.h"
 
 
 class CRoom2D : public CEntity2D
@@ -14,6 +15,8 @@ protected:
 	CRoom2D();
 	~CRoom2D();
 	std::vector<CRoom2D*> doorList;
+
+
 };
 
 class CRoomManager2D : public CSingletonTemplate<CRoomManager2D>
@@ -24,11 +27,16 @@ public:
 protected:
 	CRoomManager2D();
 	~CRoomManager2D();
-	bool getprev();
-	bool getnext();
-	void getadd();
+	void getprev();
+	void getnext();
+	void GenerateRoom();
+	void NextRoom(int door);
+	void currentRoom();
 
 	std::vector<CRoom2D*> roomList;
+
+	// The handler containing the instance of the 2D Map
+	CMap2D* cMap2D;
 
 	int cursor;
 };
